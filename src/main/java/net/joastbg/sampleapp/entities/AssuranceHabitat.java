755492f1e0a7a1da154287e3c2f83e8a76ab877b
Robin <robin.gay13@gmail.com>
2018-07-01 17:32:14 +0200

    dao assurance & entities/Assurance.java

--- src/main/java/net/joastbg/sampleapp/entities/AssuranceHabitat.java
+++ src/main/java/net/joastbg/sampleapp/entities/AssuranceHabitat.java
@@ -1,4 +1,56 @@
 package net.joastbg.sampleapp.entities;
 
-public class AssuranceHabitat {
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "ASSURANCE_HABITATION")
+public class AssuranceHabitat extends Assurance implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5645381708288410304L;
+	
+	@Column(name = "adresse")
+    private String adresse;
+	
+	@Column(name = "valeurConverture")
+    private String valeurConverture;
+	
+	 @OneToOne
+	   private Assurance assurance;
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getValeurConverture() {
+		return valeurConverture;
+	}
+
+	public void setValeurConverture(String valeurConverture) {
+		this.valeurConverture = valeurConverture;
+	}
+
+	public Assurance getAssurance() {
+		return assurance;
+	}
+
+	public void setAssurance(Assurance assurance) {
+		this.assurance = assurance;
+	}
+
+	@Override
+	public String toString() {
+		return "AssuranceHabitat [adresse=" + adresse + ", valeurConverture=" + valeurConverture + ", assurance="
+				+ assurance + "]";
+	}
+	
+	 
 }
--- src/main/java/net/joastbg/sampleapp/entities/Echeances.java
+++ src/main/java/net/joastbg/sampleapp/entities/Echeances.java
@@ -1,4 +1,78 @@
 package net.joastbg.sampleapp.entities;
 
-public class Echeances {
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="ECHEANCES")
+public class Echeances implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1088262973262594032L;
+	
+	@Column(name = "prix")
+    private String prix;
+	
+	@Column(name = "dateEmission")
+    private String dateEmission;
+	
+	@Column(name = "datePaiement")
+    private String datePaiement;
+	
+	@Column(name = "dateEmissionFacture")
+    private String dateEmissionFacture;
+	
+	@OneToOne
+	   private Assurance assurance;
+
+	public String getPrix() {
+		return prix;
+	}
+
+	public void setPrix(String prix) {
+		this.prix = prix;
+	}
+
+	public String getDateEmission() {
+		return dateEmission;
+	}
+
+	public void setDateEmission(String dateEmission) {
+		this.dateEmission = dateEmission;
+	}
+
+	public String getDatePaiement() {
+		return datePaiement;
+	}
+
+	public void setDatePaiement(String datePaiement) {
+		this.datePaiement = datePaiement;
+	}
+
+	public String getDateEmissionFacture() {
+		return dateEmissionFacture;
+	}
+
+	public void setDateEmissionFacture(String dateEmissionFacture) {
+		this.dateEmissionFacture = dateEmissionFacture;
+	}
+
+	public Assurance getAssurance() {
+		return assurance;
+	}
+
+	public void setAssurance(Assurance assurance) {
+		this.assurance = assurance;
+	}
+
+	@Override
+	public String toString() {
+		return "Echeances [prix=" + prix + ", dateEmission=" + dateEmission + ", datePaiement=" + datePaiement
+				+ ", dateEmissionFacture=" + dateEmissionFacture + ", assurance=" + assurance + "]";
+	}
+	
+	
 }
