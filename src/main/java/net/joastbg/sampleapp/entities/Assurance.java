d06f237e44ec27f1d9b35b7cc7998da7c1033bbd
Robin <robin.gay13@gmail.com>
2018-07-01 14:29:01 +0200

    commit test

---
+++ src/main/java/net/joastbg/sampleapp/entities/Assurance.java
@@ -0,0 +1,16 @@
+package net.joastbg.sampleapp.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+
+public abstract class Assurance implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 574779180656266341L;
+
+
+}
